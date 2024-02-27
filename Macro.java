af2b54f1866f4d006ec95993b9262118865824bd
Frank <franklinsales1711@gmail.com>
2024-02-27 01:29:50 -0300

    Remoção de arquivos desnecessáriso

--- Macro.java
+++ Macro.java
@@ -1,33 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+public class Macro{
+    Tables table;
+    String name;
 
-public class Macro {
-    private String name;
-    private String[] parameters;
-    private List<String> lines;
-
-    public Macro(String name) {
+    // Construtor que recebe uma instância existente de Tables
+    public Macro(Tables table, String name) {
+        this.table = table;
         this.name = name;
-        this.lines = new ArrayList<>();
     }
 
+    // Construtor que cria uma nova instância de Tables
+    public Macro() {
+        this.table = new Tables();
+        this.name = new String();
+    }
+    // Método para obter o valor de name
     public String getName() {
         return name;
     }
 
-    public void addLine(String line) {
-        lines.add(line);
+    // Método para definir o valor de name
+    public void setName(String name) {
+        this.name = name;
+    }
+    // Método para adicionar uma string à defTab da instância de Tables
+    public void addStringToDefTab(String newString) {
+        table.addStringToDefTab(newString);
+    }
+    public String getDeftaString(){
+        return table.getDefTab();
     }
 
-    public List<String> getLines() {
-        return lines;
+
+    /*
+     * Aqui ocorre a segunda fase do desenolvimento
+     * é feita a substituição de parametro por argumento
+     * 
+     */
+    public void addArgTab(String line){
+        table.addArgTab(line);
+    }
+    public String getArgTab(){
+        return table.getAgrTab();
     }
 
-    public void setParameters(String[] parameters) {
-        this.parameters = parameters;
+    // obter parametros
+    public void setParameters(String line) {
+        // encontra a posição do primeiro ""&""
+        table.setParameters(line);
+
+    }
+    public String getParamenters() {
+        return table.getParamenters();
     }
 
-    public String[] getParameters() {
-        return parameters;
+    // Método para imprimir o conteúdo de defTab da instância de Tables
+    public void imprimirDefTab() {
+        table.imprimirDefTab();
     }
+   
 }
